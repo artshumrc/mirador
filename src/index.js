51f624e3fd102c03b3546b0cda32a9ff5c8d11ca
Rashmi Singhal <rashmi.s.singhal@gmail.com>
2019-09-04 16:16:35 -0400

    add more imports

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import init from './init';
 import * as actions from './state/actions';
 import * as selectors from './state/selectors';
+import '@babel/polyfill';
+import 'url-polyfill/url-polyfill';
+import 'unfetch/polyfill';
 
 export * from './state/reducers';
 
